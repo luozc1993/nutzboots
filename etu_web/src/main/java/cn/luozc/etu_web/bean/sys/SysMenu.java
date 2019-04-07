61b01456547589593652b3386d14dbf46bfb93d4
Administrator <1079648153@qq.com>
2019-04-07 22:17:27 +0800

    菜单管理 菜单添加

---
+++ etu_web/src/main/java/cn/luozc/etu_web/bean/sys/SysMenu.java
@@ -0,0 +1,118 @@
+package cn.luozc.etu_web.bean.sys;
+
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Name;
+import org.nutz.dao.entity.annotation.Table;
+
+import java.util.Date;
+
+@Table("sys_menu")
+public class SysMenu {
+
+  @Name
+  private String id;
+  @Column
+  private String name;
+  @Column
+  private String url;
+  @Column("parent_id")
+  private String parentId;
+  @Column
+  private String icon;
+  @Column("is_menu")
+  private long isMenu;
+  @Column
+  private long sort;
+  @Column("create_time")
+  private Date createTime;
+  @Column("update_time")
+  private Date updateTime;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public String getParentId() {
+    return parentId;
+  }
+
+  public void setParentId(String parentId) {
+    this.parentId = parentId;
+  }
+
+  public String getIcon() {
+    return icon;
+  }
+
+  public void setIcon(String icon) {
+    this.icon = icon;
+  }
+
+  public long getIsMenu() {
+    return isMenu;
+  }
+
+  public void setIsMenu(long isMenu) {
+    this.isMenu = isMenu;
+  }
+
+  public long getOrder() {
+    return sort;
+  }
+
+  public void setOrder(long order) {
+    this.sort = order;
+  }
+
+  public Date getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(Date createTime) {
+    this.createTime = createTime;
+  }
+
+  public Date getUpdateTime() {
+    return updateTime;
+  }
+
+  public void setUpdateTime(Date updateTime) {
+    this.updateTime = updateTime;
+  }
+
+
+  @Override
+  public String toString() {
+    return "SysMenu{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            ", url='" + url + '\'' +
+            ", parentId='" + parentId + '\'' +
+            ", icon='" + icon + '\'' +
+            ", isMenu=" + isMenu +
+            ", order=" + sort +
+            ", createTime=" + createTime +
+            ", updateTime=" + updateTime +
+            '}';
+  }
+}
